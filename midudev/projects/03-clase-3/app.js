c2e90c553b559d5b9c7ad2b4d1e779fbd166f387
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-03 09:59:37 -0300

    generamos la ruta para recuperar todas las peliculas

--- midudev/projects/03-clase-3/app.js
+++ midudev/projects/03-clase-3/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const movies = require("./movies.json");
 
 const app = express();
 app.disable("x-powered-by");
@@ -6,6 +7,13 @@ app.disable("x-powered-by");
 app.get("/", (req, res) => {
   res.json({ message: "hola mundo" });
 });
+
+app.get("/movies", (req, res) => {
+  {
+    res.json(movies);
+  }
+});
+
 
 const PORT = process.env.PORT ?? 1234;
 
