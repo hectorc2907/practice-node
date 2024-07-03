2ecb29403c38a70902002bbd7cd1c20652fa488a
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-03 10:06:34 -0300

    generamos la ruta para filtro de peliculas por id

--- midudev/projects/03-clase-3/app.js
+++ midudev/projects/03-clase-3/app.js
@@ -4,16 +4,17 @@ const movies = require("./movies.json");
 const app = express();
 app.disable("x-powered-by");
 
-app.get("/", (req, res) => {
-  res.json({ message: "hola mundo" });
-});
-
 app.get("/movies", (req, res) => {
-  {
-    res.json(movies);
-  }
+  res.json(movies);
 });
 
+app.get("/movies/:id", (req, res) => {
+  // path-to-regexp
+  const { id } = req.params;
+  const movie = movies.find((movie) => movie.id === id);
+  if (movie) return res.json(movie);
+  res.status(404).json({ message: "Movie not found" });
+});
 
 const PORT = process.env.PORT ?? 1234;
 
