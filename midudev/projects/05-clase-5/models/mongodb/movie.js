0f8cc3edd9616820b2ee1a895cacdeffc79abf0c
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-05 23:34:02 -0300

    se realiza el modelo de actualizacion de las peliculas por mongodb

--- midudev/projects/05-clase-5/models/mongodb/movie.js
+++ midudev/projects/05-clase-5/models/mongodb/movie.js
@@ -55,4 +55,15 @@ export class MovieModel {
     const { deletedCount } = await db.deleteOne({ _id: objectId });
     return deletedCount > 0;
   }
+  static async update({ id, input }) {
+    const db = await connect();
+    const objectId = new ObjectId(id);
+    const { ok, value } = await db.findOneAndUpdate(
+      { _id: objectId },
+      { $set: input },
+      { returnNewDocument: true }
+    );
+    if (!ok) return false;
+    return value;
+  }
 }
