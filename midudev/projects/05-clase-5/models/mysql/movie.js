90c807d12eb2dd6bedeaea62903ff07df24841dc
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-07 10:45:19 -0300

    se configura el getAll para mysql

--- midudev/projects/05-clase-5/models/mysql/movie.js
+++ midudev/projects/05-clase-5/models/mysql/movie.js
@@ -11,3 +11,30 @@ const DEFAULT_CONFIG = {
 const connectionString = process.env.DATABASE_URL ?? DEFAULT_CONFIG;
 
 const connection = await mysql.createConnection(connectionString);
+
+export class MovieModel {
+  static async getAll({ genre }) {
+    console.log("getAll");
+    if (genre) {
+      const lowerCaseGenre = genre.toLowerCase();
+      // get genre ids from database table using genre names
+      const [genres] = await connection.query(
+        "SELECT id, name FROM genre WHERE LOWER(name) = ?;",
+        [lowerCaseGenre]
+      );
+      // no genre found
+      if (genres.length === 0) return [];
+      // get the id from first genre result
+      const [{ id }] = genres;
+      // get all movies ids from database table
+      // la query a movie_genres
+      // join
+      // y devolver resultados..
+      return [];
+    }
+    const [movies] = await connection.query(
+      "SELECT title, year, director, duracion, poster, rate, BIN_TO_UUID(id) id FROM movie;"
+    );
+    return movies;
+  }
+}
