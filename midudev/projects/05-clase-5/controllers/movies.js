c522d4c2b1aa7907874b2ccb5d590d27661b0c7a
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-07 11:16:21 -0300

    se genera el controlador para obtener una pelicula por id

--- midudev/projects/05-clase-5/controllers/movies.js
+++ midudev/projects/05-clase-5/controllers/movies.js
@@ -9,4 +9,10 @@ export class MovieController {
     const movies = await this.movieModel.getById({ genre });
     res.json(movies);
   };
+  getById = async (req, res) => {
+    const { id } = req.params;
+    const movie = await this.movieModel.getById({ id });
+    if (movie) return res.json(movie);
+    res.status(404).json({ message: "Movie not found" });
+  };
 }
