75c199f42f625e7e56caed4d2402ce102ae8417b
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-07 11:14:14 -0300

    se realiza el controlador para obtener todas las peliculas

--- midudev/projects/05-clase-5/controllers/movies.js
+++ midudev/projects/05-clase-5/controllers/movies.js
@@ -4,4 +4,9 @@ export class MovieController {
   constructor({ movieModel }) {
     this.movieModel = movieModel;
   }
+  getAll = async (req, res) => {
+    const { genre } = req.params;
+    const movies = await this.movieModel.getById({ genre });
+    res.json(movies);
+  };
 }
