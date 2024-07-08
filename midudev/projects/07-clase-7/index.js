892e00f20f3ecabb489b554876928c6e09e79e37
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-08 09:38:46 -0300

    levantamos el servidor en el puerto 3000, creamos config.js para tener las configuraciones ahi

--- midudev/projects/07-clase-7/index.js
+++ midudev/projects/07-clase-7/index.js
@@ -1,1 +1,12 @@
-console.log("start")
+import express from "express";
+import { PORT } from "./config.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port:${PORT}`);
+});
