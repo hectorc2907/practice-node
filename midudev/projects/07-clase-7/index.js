245b1d7f3bb1cc68091bb0e4ef9e80678a079925
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-08 09:43:16 -0300

    complemento con logger para poder las peticiones en la consola

--- midudev/projects/07-clase-7/index.js
+++ midudev/projects/07-clase-7/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import logger from "morgan";
 import { PORT } from "./config.js";
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
