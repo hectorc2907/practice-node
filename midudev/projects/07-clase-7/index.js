8779366c8b917f2d5254ca977a6011df21d6e12d
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-08 12:13:32 -0300

    renderizamos una plantilla para simular el frontend y lo adaptamos para que la aplicacion corra

--- midudev/projects/07-clase-7/index.js
+++ midudev/projects/07-clase-7/index.js
@@ -4,12 +4,13 @@ import { PORT } from "./config.js";
 import { UserRepository } from "./user-repository.js";
 
 const app = express();
+app.set("view engine", "ejs");
 app.use(express.json());
 
 app.use(logger("dev"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
 });
 
 app.post("/login", async (req, res) => {
